58edbe2bd3555d04239ca4d1564f7d7ce77cae53
guofucheng <252879395@qq.com>
2018-10-28 21:55:15 +0800

    第一次提交了

---
+++ src/com/itheima/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+    }
+}
